c52d819c6a1928f5d4d0af7d30ae4094647c530e
Javier Elio <javiereliomedina@gmail.com>
2022-04-11 12:41:35 +0200

    First commit

---
+++ app.R
@@ -0,0 +1,115 @@
+library(shiny)
+library(supercells)
+library(terra)
+library(sf)
+library(shinybusy)
+
+distance <- c(philentropy::getDistMethods(),
+              #"jsd",
+              "dtw")
+averaging <- c("mean", "median", "max", "min")
+transformations <- c("NULL", "to_LAB")
+
+ui <- fluidPage(
+  
+  add_busy_spinner(spin = "fading-circle", color = "#98c1d9"),
+  theme = shinythemes::shinytheme("cerulean"),
+  
+  h1("Shiny implementation of supercells package"), 
+  p("This Shiny app is an implementation of the R-package",
+    a("supercells.", href = "https://jakubnowosad.com/supercells/index.html"),
+    "Please read the package manual for more information, I am only a user and
+      have no developed it (I just found it very interesting!!)"),
+  
+  fluidRow(
+    column(2,
+           # Read raster
+           fileInput("img", "Select spatial raster", accept = ".tif")
+    )
+  ),
+  
+  fluidRow(
+    
+    # Parameter for supercells
+    column(2,
+           
+           numericInput("k", "Number of supercells desired", 
+                        value = 50,
+                        min = 50, max = 5000, step = 50)
+    ),
+    column(2,
+           numericInput("compactness", "Compactness", 
+                        value = 1,
+                        min = 1, max = 20, step = 1)
+    ),
+    column(2, selectInput("dist_fun", "Distance function", distance)),
+    column(2, selectInput("avg_fun", "Averaging function", averaging)),
+    column(2, selectInput("transform", "Transform", transformations))
+    
+    
+  ),
+  
+  fluidRow(
+    column(6,
+           # Plot input raster
+           p(strong("Original raster:")),
+           plotOutput("plot_in")
+    ),
+    
+    column(6,
+           # Plot output rastrer
+           p(strong("Segmented raster:")),
+           plotOutput("plot_out"))
+  ),
+  
+  fluidRow(
+    downloadButton("download", "Save output (.gpkg)")
+  )
+)
+
+server <- function(input, output, session) {
+  
+  # Read original raster
+  imagen_in <- reactive({
+    req(input$img$datapath)
+    terra::rast(input$img$datapath)
+  })
+  
+  # Supercells
+  image_out <- reactive({
+    supercells(imagen_in(),
+               k = input$k,
+               compactness = input$compactness,
+               dist_fun = input$dist_fun,
+               avg_fun = input$avg_fun,
+               transform = input$transform
+    )
+  })
+  
+  # Plots
+  output$plot_in <- renderPlot({
+    if (is.null(imagen_in()))
+      return(NULL)
+    plot(imagen_in())
+  })
+  
+  output$plot_out <- renderPlot({
+    if (is.null(imagen_in()))
+      return(NULL)
+    plot(imagen_in())
+    plot(st_geometry(image_out()), add = TRUE, lwd = 0.2)
+  })
+  
+  # Export output
+  output$download <- downloadHandler(
+    filename = function(){
+      paste0(gsub(".tif", "", input$img), "_supercell.gpkg")
+    },
+    content = function(file){
+      write_sf(image_out(), file)
+    }
+  )
+}
+
+shinyApp(ui, server)
+
