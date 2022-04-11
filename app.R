6380c8b2e4f9c4f5af2659f0fc85f62b63cd2ed5
Javier Elio <javiereliomedina@gmail.com>
2022-04-11 12:50:04 +0200

    Add "buymeacoffe"

--- app.R
+++ app.R
@@ -64,6 +64,21 @@ ui <- fluidPage(
   
   fluidRow(
     downloadButton("download", "Save output (.gpkg)")
+  ),
+  
+  a(HTML(
+    "<script data-name=\"BMC-Widget\"
+          data-cfasync=\"false\" 
+          src=\"https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js\" 
+          data-id=\"javierelio\" 
+          data-description=\"Support me on Buy me a coffee!\"
+          data-message=\"\" 
+          data-color=\"#ff813f\" 
+          data-position=\"Right\" 
+          data-x_margin=\"18\" 
+          data-y_margin=\"18\">
+          </script>"
+  )
   )
 )
 
